18281d93d9ed065eeb5d8d5a0a348ced7b5fe278
Alinani Simukanga <a1inani@users.noreply.github.com>
2025-11-10 13:33:59 +0200

    Update database.py

--- foundrydb/database.py
+++ foundrydb/database.py
@@ -2,8 +2,9 @@ import os
 # An abstraction of a Database
 class Database:
     def __init__(self, name):
-        self._name = name
         nested_path = "foundries/"+name
+        self.name = name
+        self.path = nested_path
         try:
             os.makedirs(nested_path, exist_ok=True) # exist_ok=True prevents error if folder already exists
             print(f"FoundryDB ready at {nested_path}")
