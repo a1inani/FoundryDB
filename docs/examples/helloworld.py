021fcae14b538a906a0cb364524f5274f61747cf
Alinani Simukanga <a1inani@users.noreply.github.com>
2025-11-11 14:19:28 +0200

    Delete helloworld.py

--- docs/examples/helloworld.py
+++
@@ -1,7 +0,0 @@
-from foundrydb import Database
-
-db = Database()
-db.execute("CREATE TABLE users (id INT, name TEXT);")
-db.execute("INSERT INTO users VALUES (1, 'Alice');")
-rows = db.execute("SELECT * FROM users;")
-print(rows)
